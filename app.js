22176b81c029edfdda624f8b3ba95cc0ccc54a3a
“Minh <minhtt21@fe.edu.vn>
2022-10-11 12:01:57 +0700

    change view to ejs

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('admin', { title: 'Admin Page', name: "MINH" });
+  res.render('admin', { title: 'Admin Page', name: 'MINH' });
 });
 
 module.exports = router;
